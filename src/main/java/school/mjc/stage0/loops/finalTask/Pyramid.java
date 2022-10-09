f684725704f0926a9ff4c718cc64245e57c56f07
SardorAKayumov <sardor.zeroz@gmail.com>
2022-10-09 18:58:52 +0500

    commit

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,18 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j <= cathetusLength - i; j++)
+                System.out.print(" ");
 
+            for (int j = 2 * i - 1; j >= i; j--)
+                System.out.print(j - i + 1);
+
+            for (int j = i; j < 2 * i - 1; j++)
+                System.out.print(j - i + 2);
+
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
